63eb5ac594abf67505f563e4433ab8845fcf01a7
Rohan Ranjan <rohanranjanghosh@gmail.com>
2020-11-21 12:08:04 +0530

    first commit: first prototype

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    console.log(firstName + " " + lastName + " " + email);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Listening to Port: "+port);
+});
